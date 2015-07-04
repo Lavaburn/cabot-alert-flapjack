102d948ef024e98a1fd63cae38d1858169027960
Lavaburn <nicolas@truyens.com>
2015-07-04 17:12:42 +0200

    Bugfix for initial test

--- cabot_alert_flapjack/models.py
+++ cabot_alert_flapjack/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+#from django.conf import settings
+#from django.template import Context, Template
+
 from cabot.cabotapp.alert import AlertPlugin, AlertPluginUserData
+
+from os import environ as env
+
+#import requests
+
+#sb_alert_url = env['STASHBOARD_URI']
 
 class FlapjackAlert(AlertPlugin):
     name = "Flapjack"
@@ -14,7 +23,6 @@ class FlapjackAlert(AlertPlugin):
         
         return
 
-class FlapjackAlertUserData(AlertPluginUserData):
-    name = "Flapjack Plugin"
-    #favorite_bone = models.CharField(max_length=50, blank=True)
-
+# class FlapjackAlertUserData(AlertPluginUserData):
+#     name = "Flapjack Plugin"
+#     favorite_bone = models.CharField(max_length=50, blank=True)
